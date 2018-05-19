80b7e87a9125b16f99684ba9d6c2b07dcdc19608
Pau Giner <pau.giner@gmail.com>
2018-05-19 18:16:02 +0200

    Autofocus adjustments

--- mobile-translation/script/compose.js
+++ mobile-translation/script/compose.js
@@ -4,9 +4,14 @@ data: function(){
       data: data
     };
   },
+  mounted: function(){
+    var tr = this.$refs.translation;
+    tr.focus();
+    window.scrollTo(0, 0);
+  },
   methods: {
    next: function () {
-     
+
    },
    back: function() {
      goToArticle();
