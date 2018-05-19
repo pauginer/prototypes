22d1dfabaada4c538e8097ccb9863034c739d9de
Pau Giner <pau.giner@gmail.com>
2018-05-19 14:46:50 +0200

    MT simulated in the prototype

--- mobile-translation/script/data.js
+++ mobile-translation/script/data.js
@@ -6,5 +6,15 @@ var data = {
   ],
   seenTutorial: false,
   selection: 0,
-  maxSelection: 7
+  maxSelection: 7,
+  targetLang: "es",
+  mt: {es:["<b>Las plantas en el espacio</b> son <a href='#'>plantas</a> cultivadas en el <a href='#'>espacio exterior</a>.",
+  "A fines del siglo XX y en el siglo XXI, las plantas a menudo se llevaban al espacio en órbita baja de la Tierra para cultivarse en un entorno controlado, pero sin presión, a veces llamado jardín espacial.",
+  "En el contexto del vuelo espacial humano, se pueden consumir como alimento y / o proporcionar una atmósfera refrescante.",
+  "Las plantas pueden metabolizar el dióxido de carbono en el aire para producir oxígeno valioso y pueden ayudar a controlar la humedad de la cabina.",
+  "El crecimiento de las plantas en el espacio puede proporcionar un beneficio psicológico para los tripulantes de vuelos espaciales humanos.",
+  "Usualmente los planetas eran parte de estudios o desarrollo técnico para desarrollar jardines espaciales o realizar experimentos científicos.",
+  "Hasta la fecha, las plantas llevadas al espacio han tenido mayor interés científico, con contribuciones limitadas a la funcionalidad de la nave espacial, sin embargo, el proyecto del árbol Luna Apolo fue una misión más o menos inspirada en la silvicultura y los árboles parte de la celebración del bicentenario de un país."
+    ]
+  }
 };
